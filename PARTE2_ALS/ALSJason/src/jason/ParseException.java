081d3ea6fb7b11d759769dfc1af6fd2995d034d5
VictorPS <victor_toguro@hotmail.com>
2016-05-25 13:25:14 -0300

    Revert "ParseException error message translated to pt-br"
    This reverts commit b48a92cd3ab96a223a2fbb526988d8de9296b172.

--- PARTE2_ALS/ALSJason/src/jason/ParseException.java
+++ PARTE2_ALS/ALSJason/src/jason/ParseException.java
@@ -103,7 +103,7 @@ public class ParseException extends Exception {
       }
       expected.append(eol).append("    ");
     }
-    String retval = "Encontrado ";
+    String retval = "Encountered \"";
     Token tok = currentToken.next;
     for (int i = 0; i < maxSize; i++) {
       if (i != 0) retval += " ";
@@ -111,20 +111,18 @@ public class ParseException extends Exception {
         retval += tokenImage[0];
         break;
       }
-      retval += "\"";
+      retval += " " + tokenImage[tok.kind];
+      retval += " \"";
       retval += add_escapes(tok.image);
-      retval += "\" (";
-      retval += tokenImage[tok.kind];
-      retval += ")";
-      
+      retval += " \"";
       tok = tok.next;
     }
-    retval += " na linha " + currentToken.next.beginLine + ", coluna " + currentToken.next.beginColumn;
+    retval += "\" at line " + currentToken.next.beginLine + ", column " + currentToken.next.beginColumn;
     retval += "." + eol;
     if (expectedTokenSequences.length == 1) {
-      retval += "Estava esperando:" + eol + "    ";
+      retval += "Was expecting:" + eol + "    ";
     } else {
-      retval += "Estava esperando um dos abaixo:" + eol + "    ";
+      retval += "Was expecting one of:" + eol + "    ";
     }
     retval += expected.toString();
     return retval;
